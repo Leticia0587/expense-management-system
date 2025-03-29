e46b635ef2f548e76724526cddc515b306bb9bd9
Leticia0587 <leticiadearaujo005@gmail.com>
2025-03-28 21:21:22 -0300

    feat: integração do Swagger com Springdoc OpenAPI para documentar a API

--- src/main/java/com/projeto/finance_manager/model/User.java
+++ src/main/java/com/projeto/finance_manager/model/User.java
@@ -1,11 +1,16 @@
 package com.projeto.finance_manager.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "users")
-
+@Getter
+@Setter
+@NoArgsConstructor
 public class User {
 
     @Id
@@ -15,52 +20,19 @@ public class User {
     private String name;
     private String email;
     private String password;
-
     private LocalDateTime createdAt;
 
-    //Construtor padrão obrigatorio JPA
-    public User() {
-    }
-
-
-        //Construtor para facilitar a criação de usuários
+    // Construtor para facilitar a criação de usuários
     public User(String name, String email, String password) {
-            this.name = name;
-            this.email = email;
-            this.password = password;
-            this.createdAt = LocalDateTime.now();
-        }
-
-        //Getters e Setters para acessar os atributos de forma controlada
-        public Long getId() {
-            return id;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name){
-            this.name = name;
-        }
-
-        public String getEmail() {
-            return email;
-        }
-
-        public void setEmail(String email){
-            this.email = email;
-        }
-
-        public String getPassword() {
-            return password;
-        }
-
-        public void setPassword(String password){
-            this.password = password;
-        }
-
-        public LocalDateTime getCreatedAt() {
-            return createdAt;
-        }
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.createdAt = LocalDateTime.now();
     }
+
+    // Garante que o createdAt seja preenchido antes de salvar no banco
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
--- src/main/java/com/projeto/finance_manager/service/UserService.java
+++ src/main/java/com/projeto/finance_manager/service/UserService.java
@@ -6,7 +6,6 @@ import com.projeto.finance_manager.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class UserService {
