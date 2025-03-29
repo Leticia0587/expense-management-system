0d5f91ff1c0c7c211a5bb71c28d55473de845e23
Leticia0587 <leticiadearaujo005@gmail.com>
2025-03-28 21:27:57 -0300

    feat: configuração de CORS para permitir comunicação entre frontend e backend

---
+++ src/main/java/com/projeto/finance_manager/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.projeto.finance_manager.config;
+
+import org.apache.catalina.filters.CorsFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+        @Bean
+        public CorsFilter corsFilter() {
+            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+            CorsConfiguration config = new CorsConfiguration();
+
+            config.setAllowedOrigins(List.of("*")); // Permite qualquer origem
+            config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+            config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+
+            source.registerCorsConfiguration("/**", config);
+            return new CorsFilter();
+        }
+    }
+
