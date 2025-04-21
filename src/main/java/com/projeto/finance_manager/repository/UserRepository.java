e9bc9864b6304f041eb576112ea2c1f304e1b3d1
Leticia0587 <leticiadearaujo005@gmail.com>
2025-04-21 19:06:39 -0300

    chore: ajustes gerais no UserRepository

--- src/main/java/com/projeto/finance_manager/repository/UserRepository.java
+++ src/main/java/com/projeto/finance_manager/repository/UserRepository.java
@@ -10,5 +10,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
-    Optional<Object> findByUsername(String username);
+    Optional<User> findByUsername(String username);
 }
