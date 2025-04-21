e1046379344cd0aaa703b092c35059af55fe2aa6
Leticia0587 <leticiadearaujo005@gmail.com>
2025-04-21 19:05:32 -0300

    fix: ajusta campos e estrutura da model Expense

--- src/main/java/com/projeto/finance_manager/model/Expense.java
+++ src/main/java/com/projeto/finance_manager/model/Expense.java
@@ -1,25 +1,59 @@
 package com.projeto.finance_manager.model;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
-@Setter
-@Getter
-@Entity // Essa anotação é obrigatória
-@Table(name = "expenses")
+@Entity
 public class Expense {
 
-    // Getters e Setters
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String description;
-    private Double value;
+    private BigDecimal value;
     private LocalDate date;
     private String category;
 
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
