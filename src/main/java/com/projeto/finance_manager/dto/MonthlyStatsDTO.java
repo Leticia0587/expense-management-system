35b95d079eaaea5b27fa094c32d48f9d06eeed63
Leticia0587 <leticiadearaujo005@gmail.com>
2025-04-21 19:04:24 -0300

    feat: adiciona DTO para estatísticas mensais

--- src/main/java/com/projeto/finance_manager/dto/MonthlyStatsDTO.java
+++ src/main/java/com/projeto/finance_manager/dto/MonthlyStatsDTO.java
@@ -1,2 +1,34 @@
-package com.projeto.finance_manager.dto;public class MonthlyStatsDTO {
+package com.projeto.finance_manager.dto;
+
+import java.math.BigDecimal;
+
+public class MonthlyStatsDTO {
+
+    private String month;
+    private BigDecimal totalValue;
+    private Long totalExpenses;
+    private BigDecimal averageValue;
+
+    public MonthlyStatsDTO(String month, BigDecimal totalValue, Long totalExpenses, BigDecimal averageValue) {
+        this.month = month;
+        this.totalValue = totalValue;
+        this.totalExpenses = totalExpenses;
+        this.averageValue = averageValue;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public BigDecimal getTotalValue() {
+        return totalValue;
+    }
+
+    public Long getTotalExpenses() {
+        return totalExpenses;
+    }
+
+    public BigDecimal getAverageValue() {
+        return averageValue;
+    }
 }
